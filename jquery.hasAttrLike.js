f3b3d83c19136235e150f48fceaf4aaff1f1bcd2
William Bert <william@spanishdict.com>
2014-10-21 17:47:33 -0700

    Search class by default.

--- jquery.hasAttrLike.js
+++ jquery.hasAttrLike.js
@@ -1,6 +1,10 @@
 // Select elements by testing each value of each element's attribute `attr` for `pattern`.
 (function($) {
   $.fn.hasAttrLike = function(attr, pattern) {
+    if(pattern === undefined) {
+      pattern = attr;
+      attr = "class";
+    }
     return this.filter(function(idx) {
       var elAttr = $(this).attr(attr);
       if(!elAttr) return false;
